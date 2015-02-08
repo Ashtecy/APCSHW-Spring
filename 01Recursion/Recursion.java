f31f0c8d3fe94d3bc095371f329c4327b7873035
Rccyl <Raashid224@aol.com>
2015-02-07 19:27:04 -0500

    Recursion

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -1,2 +1,35 @@
 public class Recursion{
+
+    public String name(){
+	return "Chowdhury,Raashid";
+    }
+
+    public int fact(int n){
+	if(n==1){
+	    return 1;
+	}
+	return n*fact(n-1);
+    }
+
+    public int fib(int n){
+	if(n==0){
+	    return 0;
+	}
+	if(n==1){
+	    return 1;
+	}
+	return fib(n-1)+fib(n-2);
+    }
+
+    public double sqrt(double n){
+	return sqrt2(n,1);
+    }
+
+    public double sqrt2(double n,double guess){
+	if(guess*guess==n){
+	    return guess;
+	}
+	return sqrt2(n,(n/guess+guess)/2);
+    }
+    
 }
