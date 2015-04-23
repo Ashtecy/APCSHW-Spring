fd542af645393f2700b55b208313c6f1d735ff10
Raashid <raashidchowdhury@gmail.com>
2015-04-23 12:18:39 -0400

    .

---
+++ 12BTree/TreeNode.java
@@ -0,0 +1,27 @@
+public class TreeNode<E>{
+    
+    private E data;
+    private TreeNode[] children=new TreeNode[2];
+
+    public TreeNode(E data){
+	setData(data);
+    }
+    public E getData(){
+	return data;
+    }
+    public TreeNode<E> getFirst(){
+	return children[0];
+    }
+    public TreeNode<E> getSecond(){
+	return children[1];
+    }
+    public void setData(E data){
+	this.data=data;
+    }
+    public void setFirst(TreeNode<E> child){
+	children[0]=child;
+    }
+    public void setSecond(TreeNode<E> child){
+	children[1]=child;
+    }
+}
